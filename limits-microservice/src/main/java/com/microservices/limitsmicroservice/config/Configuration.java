30cceb0439dc7094ed87b7ee2a136c8bd1747c50
Ankita Singh <ankita.si2503@gmail.com>
2023-02-24 17:35:33 +0100

    new changes

--- limits-microservice/src/main/java/com/microservices/limitsmicroservice/config/Configuration.java
+++ limits-microservice/src/main/java/com/microservices/limitsmicroservice/config/Configuration.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("limit-service")
+@ConfigurationProperties("limits-service")
 @Setter
 @Getter
 public class Configuration {
