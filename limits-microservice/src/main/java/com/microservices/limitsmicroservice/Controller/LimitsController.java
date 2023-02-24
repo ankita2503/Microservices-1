86dbd500a9f0da83cdb4280e64c61725b72a2d4a
Ankita Singh <ankita.si2503@gmail.com>
2023-02-24 12:40:54 +0100

    added spring config server

---
+++ limits-microservice/src/main/java/com/microservices/limitsmicroservice/Controller/LimitsController.java
@@ -0,0 +1,2 @@
+package com.microservices.limitsmicroservice.Controller;public class LimitsController {
+}
---
+++ limits-microservice/src/main/java/com/microservices/limitsmicroservice/config/Configuration.java
@@ -0,0 +1,2 @@
+package com.microservices.limitsmicroservice.config;public class Configuration {
+}
